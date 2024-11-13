41c8d2155f3f92d03921c4fcf8bf39ab5c2a711d
FlyLeap12 <sultan_mikail_32rpl@student.smktelkom-mlg-sch.id>
2024-11-13 09:38:38 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/newlibrary123/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.newlibrary123
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/controllers/perpus_controller.dart
@@ -0,0 +1,23 @@
+
+// perpustakaan_controller.dart
+import 'package:newlibrary123/models/perpus.dart';
+
+class PerpustakaanController {
+  List<Perpus> bukuList = [];
+
+  void tambahBuku(Perpus buku) {
+    bukuList.add(buku); 
+  }
+
+  void editBuku(int index, Perpus buku) {
+    bukuList[index] = buku;
+  }
+
+  void hapusBuku(int index) {
+    bukuList.removeAt(index);
+  }
+
+  List<Perpus> getBukuList() {
+    return bukuList;
+  }
+}
